a026cd60c72dfaad6bdf2c7705bc77002c634276
superkidsyuvan <meetnandhini@outlook.com>
2024-03-30 11:35:02 -0500

    ampogus ඞඞ 😱

--- COOLEST APP/Form1.Designer.cs
+++ COOLEST APP/Form1.Designer.cs
@@ -20,6 +20,7 @@
             base.Dispose(disposing);
         }
 
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -34,6 +35,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.dwnbtn = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.menubtn = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -52,7 +61,7 @@
             // textBox
             // 
             this.textBox.AutoSize = true;
-            this.textBox.Location = new System.Drawing.Point(29, 42);
+            this.textBox.Location = new System.Drawing.Point(28, 38);
             this.textBox.Name = "textBox";
             this.textBox.Size = new System.Drawing.Size(35, 13);
             this.textBox.TabIndex = 1;
@@ -62,54 +71,143 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Location = new System.Drawing.Point(3, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.Size = new System.Drawing.Size(175, 23);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Dont click me";
+            this.button2.Text = "Change Installation Location";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 42);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(3, 29);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.Size = new System.Drawing.Size(109, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "new";
+            this.label2.Text = "Please Select a Path.";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // dwnbtn
             // 
-            this.dwnbtn.Location = new System.Drawing.Point(171, 12);
+            this.dwnbtn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dwnbtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dwnbtn.Location = new System.Drawing.Point(612, 387);
             this.dwnbtn.Name = "dwnbtn";
-            this.dwnbtn.Size = new System.Drawing.Size(75, 23);
+            this.dwnbtn.Size = new System.Drawing.Size(116, 43);
             this.dwnbtn.TabIndex = 4;
-            this.dwnbtn.Text = "download ";
-            this.dwnbtn.UseVisualStyleBackColor = true;
+            this.dwnbtn.Text = "Install";
+            this.dwnbtn.UseVisualStyleBackColor = false;
             this.dwnbtn.Click += new System.EventHandler(this.dwnbtn_Click);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(171, 42);
+            this.progressBar1.Location = new System.Drawing.Point(15, 394);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(162, 23);
+            this.progressBar1.Size = new System.Drawing.Size(521, 30);
             this.progressBar1.TabIndex = 5;
+            this.progressBar1.Visible = false;
+            // 
+            // menubtn
+            // 
+            this.menubtn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.menubtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.menubtn.Location = new System.Drawing.Point(723, 387);
+            this.menubtn.Name = "menubtn";
+            this.menubtn.Size = new System.Drawing.Size(38, 43);
+            this.menubtn.TabIndex = 6;
+            this.menubtn.Text = "☰";
+            this.menubtn.UseVisualStyleBackColor = false;
+            this.menubtn.Click += new System.EventHandler(this.menubtn_Click);
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel.Controls.Add(this.button4);
+            this.panel.Controls.Add(this.button2);
+            this.panel.Controls.Add(this.label2);
+            this.panel.Location = new System.Drawing.Point(580, 281);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(181, 100);
+            this.panel.TabIndex = 7;
+            this.panel.Visible = false;
+            this.panel.MouseLeave += new System.EventHandler(this.panel_Exit);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Downloading Game Resources 34% (234.54MB/650.51MB)";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 427);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Download Speed 8.47MB/s";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(404, 427);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Estimated remaining time: 0h 0m 0s";
+            this.label4.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(542, 395);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(33, 27);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "┃┃";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Clear Downloaded Resources";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.menubtn);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.dwnbtn);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,6 +221,13 @@
         private System.Windows.Forms.Button dwnbtn;
         private System.Windows.Forms.ProgressBar progressBar1;
         protected System.Windows.Forms.Label textBox;
+        private System.Windows.Forms.Button menubtn;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- COOLEST APP/Form1.cs
+++ COOLEST APP/Form1.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 
 
@@ -17,10 +24,105 @@ namespace COOLEST_APP
     public partial class Form1 : Form
     {
         bool debounce = false;
-        string path = "new";
+        string path = "Please Select a Path.";
+        private readonly Stopwatch stopwatch = new Stopwatch();
+        private BackgroundWorker unzipWorker;
         public Form1()
         {
             InitializeComponent();
+            InitializeBackgroundWorker();
+        }
+
+
+        public static class Prompt
+        {
+            public static string ShowDialog(string text, string caption, bool tl)
+            {
+                Form prompt = new Form()
+                {
+                    Width = 500,
+                    Height = 150,
+                    FormBorderStyle = FormBorderStyle.FixedDialog,
+                    Text = caption,
+                    StartPosition = FormStartPosition.CenterScreen
+                };
+                System.Windows.Forms.Label textLabel = new System.Windows.Forms.Label() { Left = 100, Top = 20, Width = 400, Text = text };
+                System.Windows.Forms.TextBox textBox = new System.Windows.Forms.TextBox() { Left = 50, Top = 50, Width = 400 };
+                System.Windows.Forms.Button confirmation = new System.Windows.Forms.Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+                confirmation.Click += (sender, e) => { prompt.Close(); };
+                prompt.Controls.Add(confirmation);
+                prompt.Controls.Add(textLabel);
+                if (tl == true)
+                {
+                    prompt.Controls.Add(textBox);
+                }
+                if (caption == "Error!!!!")
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    Bitmap MyImage = new Bitmap("C:\\Windows\\System32\\SecurityAndMaintenance_Error.png");
+                    pictureBox.ClientSize = new Size(50, 50);
+                    pictureBox.Image = (Image) MyImage ;
+                    pictureBox.Left = 30;
+                    pictureBox.Top = 5;
+                    prompt.Controls.Add(pictureBox);
+                }
+                if (caption == "Warning..")
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    Bitmap MyImage = new Bitmap("C:\\Windows\\System32\\SecurityAndMaintenance_Alert.png");
+                    pictureBox.ClientSize = new Size(50, 50);
+                    pictureBox.Image = (Image) MyImage;
+                    pictureBox.Left = 30;
+                    pictureBox.Top = 5;
+                    prompt.Controls.Add(pictureBox);
+                }
+                if (caption == "Information??")
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    Bitmap MyImage = new Bitmap("C:\\Windows\\System32\\SecurityAndMaintenance.png");
+                    pictureBox.ClientSize = new Size(50, 50);
+                    pictureBox.Image = (Image) MyImage;
+                    pictureBox.Left = 30;
+                    pictureBox.Top = 5;
+                    prompt.Controls.Add(pictureBox);
+                }
+                prompt.AcceptButton = confirmation;
+
+                return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+            }
+        }
+
+        private void InitializeBackgroundWorker()
+        {
+            unzipWorker = new BackgroundWorker();
+            unzipWorker.WorkerReportsProgress = true;
+            unzipWorker.DoWork += UnzipWorker_DoWork;
+            unzipWorker.ProgressChanged += UnzipWorker_ProgressChanged;
+            unzipWorker.RunWorkerCompleted += UnzipWorker_RunWorkerCompleted;
+        }
+
+        private void UnzipWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string zipFilePath = path + "\\Build.zip";
+            string extractPath = path + "\\Build";
+
+            System.IO.Compression.ZipFile.ExtractToDirectory(zipFilePath, extractPath);
+        }
+
+        private void UnzipWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Update the ProgressBar value based on progress
+            progressBar1.Value = e.ProgressPercentage;
+            label1.Text = "Unzipping Game Resources "+ e.ProgressPercentage + "%";
+        }
+
+        private void UnzipWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // Unzipping completed
+            //MessageBox.Show("Unzipping completed!");
         }
 
 
@@ -40,28 +142,107 @@ namespace COOLEST_APP
             System.Windows.Forms.DialogResult result = Dialog.ShowDialog(this);
             label2.Text = Dialog.SelectedPath;
             path = Dialog.SelectedPath;
+            if (path == "")
+            {
+                path = "Please Select a Path.";
+                label2.Text = "Path Selection was Canceled.";
+            }
+            else
+            {
+                if (Directory.Exists(path + @"\EMI\path.json" ))
+                {
 
-
+                }
+                else
+                {
+                    Console.WriteLine(path);
+                    Console.WriteLine(path + @"\EMI\path.json");
+                    Console.WriteLine(System.IO.Path.GetTempPath() + @"\EMI\path.json");
+                }
+            }
         }
 
         private void dwnbtn_Click(object sender, EventArgs e)
         {
-            if(label2.Text!= "new") {
-                Console.WriteLine("started, " + DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString());
-                using (WebClient wc = new WebClient())
+            
+            if (path != "Please Select a Path.")
+            {
+                if (Directory.Exists(path))
                 {
-                    wc.DownloadProgressChanged += wc_DownloadProgressChanged;
-                    wc.DownloadFileAsync(
-                        new System.Uri("https://download1584.mediafire.com/wqub0469ph3gfLdFV4ExULnVhqKU4EWNVsM5b90IfbggAMe2W_th65TPSgJJQKiTUUSONetZzJWYPPIvDuznATEgYeX1xmcrqa105Y9AMOfB6knBxV703bRlES8rHF6XOVn9Q1RkWXF3s-ADEAHe6a-Tf6cR34FuhhnFil6JOM4/l4mzyae52w6x8ni/Build.zip"),
-                        path+ "\\Build.zip"
-                    );
+                    Console.WriteLine("Started, " + DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString());
+                    progressBar1.Visible = true;
+                    button3.Visible = true;
+                    label1.Visible = true;
+                    label3.Visible = true;
+                    label4.Visible = true;
+                    stopwatch.Restart();
+                    using (WebClient wc = new WebClient())
+                    {
+                        wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                        wc.DownloadFileAsync(
+                            new System.Uri("https://download1584.mediafire.com/ij1u51pnsm8gsSbyvQDj_QUKeEnIbGjEVDXdvkdMJh8We85-tgdMe7jzWrK2R8t406m27nBfpUrRfnkmZBdyhFlIBpw8kOq0OP1R05Wy8vsebWYyQt33TvUHuQAlxzh-sl-mPGw8foF1cuQo7EqYnXW3HUF-A68z4NAN9Q14VDQ/l4mzyae52w6x8ni/Build.zip"),
+                            path + "\\Build.zip"
+                        );
+                    }
                 }
             }
+            else
+            {
+                string promptValue = Prompt.ShowDialog("Please Select a Path to Install Game via the '☰' button", "Error!!!!", false);
+                //throw new CustomException("Please Select a Path to Install Game via the '☰' button");
+            }
+        }
+
+        private void menubtn_Click(object sender, EventArgs e)
+        {
+            if (panel.Visible == false)
+            {
+                panel.Visible = true;
+                return;
+            }
+
+            if (panel.Visible == true)
+            {
+                panel.Visible = false;
+                return;
+            }
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void panel_Exit(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void startUnzip()
+        {
+
+            progressBar1.Value = 0;
+            label1.Text = "Initializing Unzip Process...";
+            label3.Visible = false;
+            label4.Visible = false;
+
+            System.Threading.Thread.Sleep(1000);
+
+            unzipWorker.RunWorkerAsync();
         }
 
         void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
             progressBar1.Value = e.ProgressPercentage;
+            label1.Text = "Downloading Game Resources " + e.ProgressPercentage.ToString() + "% (" + e.BytesReceived.ToString() + "Bytes /" + e.TotalBytesToReceive.ToString() + "Bytes )";
+            
+            long totalEstimatedMilliseconds = stopwatch.ElapsedMilliseconds * e.TotalBytesToReceive / e.TotalBytesToReceive;
+            TimeSpan remainingTime = TimeSpan.FromMilliseconds(totalEstimatedMilliseconds);
+
+            // Convert remaining time to hours, minutes, and seconds
+            string remainingTimeString = $"{remainingTime.Hours}h {remainingTime.Minutes}m {remainingTime.Seconds}s";
+            label4.Text = $"Estimated Remaining Time: {remainingTimeString}";
+
             if (e.ProgressPercentage == 100)
             {
                 if (debounce == false)
@@ -69,10 +250,9 @@ namespace COOLEST_APP
                     debounce = true;
                     Console.WriteLine("done, " + DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString());
 
-                    string zipPath = path + "\\Build.zip";
-                    string extractPath = path +"\\Build";
+                    startUnzip();
 
-                    System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, extractPath);
+                    //System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, extractPath);
 
 
                     //ZipFile.ExtractToDirectory(path + "\\Build.zip", path + "\\Build");
@@ -81,6 +261,21 @@ namespace COOLEST_APP
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
         {
 
         }
